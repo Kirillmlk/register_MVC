612dc6a439adfddaeec6518620bcc9248c978d1c
Kirillmlk <kostakov.kiirill@yandex.by>
2024-04-09 22:38:13 +0300

    add db connection

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-
 require_once "vendor/autoload.php";
+require 'APP/Database.php';
+use APP\Database;
+
+
+
+
+$database = new Database();
+$db = $database->connection();
+
+if (false) {
+    echo "Управление новостями<br /> ";
+    echo "<a href='#'>Создать новость</a>";
+} else {
+    echo "<a href='loginForm.php'>Login</a> | ";
+    echo "<a href='registerForm.php'>Register</a>";
+}
+
+
